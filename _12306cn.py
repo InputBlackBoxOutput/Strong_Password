be82544f712555412bd28f9c3ac8f424e974d6a8
Rutuparn <rutuparn.17U253@viit.ac.in>
2020-10-03 12:06:51 +0530

    Classified ;-)

--- _12306cn.py
+++ _12306cn.py
@@ -10,18 +10,24 @@ Used by www.12306.cn (China railways)
 Grading: 1-Dangerous  2-Average  3-Secure (Modified)
 Note: Special characters other then underscore not accepted by this evaluator: Special symbols ignored altogether
 '''
-def _12306cn(password):
-	pf = PasswordFeatures(password)
-	if pf.length <= 6 or pf.letters_only() or pf.numbers_only():
-		return 1
+class _123306cn(PasswordFeatures):
+	def __init__(self, password):
+		super().__init__(password)
 
-	else:
-		if pf.length > 6 and pf.alpha_count() and pf.digit_count():
-			return 3
+	def get_score(self):
+		if self.length <= 6 or self.letters_only() or self.numbers_only():
+			return 1
+
 		else:
-			return 2
-
+			if self.length > 6 and self.alpha_count() and self.digit_count():
+				return 3
+			else:
+				return 2
+				
+#----------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-	print(_12306cn("p@ssw0rd"))
-#-----------------------------------------------------------------------------------------------------
+	cn = _123306cn("Passw0rd")
+	print(cn.get_score())
+
+#----------------------------------------------------------------------------------------------
 #EOF
--- bloomberg.py
+++ bloomberg.py
@@ -8,29 +8,33 @@ from passwd import PasswordFeatures
 Used by www.bloomberg.com (Media company)
 Grading: 0-Too short 1-Weak 2-Good 3-Strong
 '''
-def bloomberg(password):
-	pf = PasswordFeatures(password)
-	c = 0
-	if pf.length > 5:
-		if pf.lowercase_count():
-			c += 1
-		if pf.uppercase_count():
-			c += 1
-		if pf.digit_count():
-			c += 3
-		if pf.special_count(custom="#$%&*()!@'") != 0:
-			c += 4
+class Bloomberg(PasswordFeatures):
+	def __init__(self, password):
+		super().__init__(password)
 
-	if c == 0:
-		return 0
-	elif c > 0 and c < 4:
-		return 1
-	elif c >= 4 and c <=7:
-		return 2
-	else:
-		return 3
+	def get_score(self):
+		c = 0
+		if self.length > 5:
+			if self.lowercase_count():
+				c += 1
+			if self.uppercase_count():
+				c += 1
+			if self.digit_count():
+				c += 3
+			if self.special_count(custom="#$%&*()!@'") != 0:
+				c += 4
+
+		if c == 0:
+			return 0
+		elif c > 0 and c < 4:
+			return 1
+		elif c >= 4 and c <=7:
+			return 2
+		else:
+			return 3
 
 if __name__ == "__main__":
-	print(bloomberg("abc12345"))
+	bloomberg = Bloomberg("abc12345")
+	print(bloomberg.get_score())
 #-----------------------------------------------------------------------------------------------------
 #EOF
